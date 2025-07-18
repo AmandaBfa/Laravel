fe49ad4f831114e0521dbc8b92667077e0e136f9
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-18 14:39:23 -0300

    006

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,3 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    echo "Hello World!!!";
-});
-
-Route::get('/about', function () {
-    echo "About us!!!";
-});
-
-// rota vai para o controllador main com a funcao index
-Route::get('/main/{value}', [MainController::class, 'index']);
-
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-
-Route::get('/page3/{value}', [MainController::class, 'page3']);
