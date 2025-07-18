1a9f1fe60e62b29e5c99ad09a2e51a50461b9f57
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-18 11:53:24 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,6 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo "About us!!!";
 });
+
+// rota vai para o controllador main com a funcao index
+Route::get('/main', [MainController::class, 'index']);
