b95eb9c53504c208ef7190fd2cde540f42126568
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-21 09:31:49 -0300

    013

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,11 +13,11 @@
                 <!-- form -->
                 <div class="row justify-content-center">
                     <div class="col-md-10 col-12">
-                        <form action="/loginSubmit" method="post">
+                        <form action="/loginSubmit" method="post" novalidate>
                             @csrf <!-- proteger seu formulário contra ataques CSRF (Cross-Site Request Forgery) -->
                             <div class="mb-3">
                                 <label for="text_username" class="form-label">Username</label>
-                                <input type="text" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}"> <!-- value e o old serve nesse caso para manter o que foi escrito sem ser apagado -->
+                                <input type="email" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}" required> <!-- value e o old serve nesse caso para manter o que foi escrito sem ser apagado -->
                                 <!-- show error -->
                                 @error('text_username')
                                 <div class="text-danger">{{ $message }}</div>
@@ -25,7 +25,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="text_password" class="form-label">Password</label>
-                                <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}">
+                                <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}" aria-required="">
                                 <!-- show error -->
                                 @error('text_password')
                                 <div class="text-danger">{{ $message }}</div>
