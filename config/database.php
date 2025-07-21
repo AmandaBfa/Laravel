7de929f1800d9b7fee7af5010bbd1970ac577745
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-21 10:09:41 -0300

    014

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+// não é a melhor pratica fazer as alterações do branco de dados aqui 
 return [
 
     /*
@@ -147,7 +148,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
