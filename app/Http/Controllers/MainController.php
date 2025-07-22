729cb60ca7cbe6b096505ed75d2af0ba79253c51
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-22 15:55:17 -0300

    035

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,6 +18,7 @@ class MainController extends Controller
         $user = User::find($id)->toArray();
         $notes = User::find($id)->notes()->get()->toArray();
 
+
         // show home view
         return view('home', ['notes' => $notes]);
     }
